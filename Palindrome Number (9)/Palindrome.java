12487717c24f5a599ed9fa153ef1d356862fbf2b
Piyush Kumar <piyushkumar2111@outlook.com>
2026-04-15 11:52:11 +0530

    Update Palindrome.java

--- Palindrome Number (9)/Palindrome.java
+++ Palindrome Number (9)/Palindrome.java
@@ -1,1 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int number) {
+        int reversed = 0;
+        int original = number;
 
+        if (number < 0) {
+            return false;
+        }
+
+        while (number != 0) {
+            reversed = (reversed * 10) + number % 10;
+            number /= 10;
+        }
+
+        if (reversed == original) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
